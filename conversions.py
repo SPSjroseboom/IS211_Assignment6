e93c4d5cc69e1bcc60bbd816511801f53c4e92f7
SPSjroseboom <36117046+SPSjroseboom@users.noreply.github.com>
2019-08-16 20:16:41 -0400

    Add files via upload

--- conversions.py
+++ conversions.py
@@ -16,12 +16,12 @@ def convertCelsiusToFahrenheit(a_float):
 
 
 def convertFahrenheitToCelsius(a_float):
-    C = (float(a_float) - 32) * 0.56
+    C = (float(a_float) - 32) / 1.8
     return round(C, 2)
 
 
 def convertFahrenheitToKelvin(a_float):
-    K = (float(a_float) - 32) * 0.56 + 273.15
+    K = (float(a_float) - 32) * 5 / 9 + 273.15
     return round(K, 2)
 
 
